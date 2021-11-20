384ee5a52baa7b282d3ea3faabcfbd842a3fe73d
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-21 00:44:09 +0300

    notes created

---
+++ src/components/DisplayNotes.js
@@ -0,0 +1,12 @@
+import { Box } from '@mui/system'
+import React from 'react'
+
+function DisplayNotes() {
+	return (
+		<Box>
+			Notes will be shown here.
+		</Box>
+	)
+}
+
+export default DisplayNotes
