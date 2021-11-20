cc5219061f1cd31ccaa07a0ea4c6d9b423ccc030
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-21 00:41:45 +0300

    note area imported

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Box } from '@mui/system';
 import './App.css';
 import Header from './components/Header';
 import SearchArea from './components/SearchArea';
+import TakeNoteArea from './components/TakeNoteArea';
 
 function App() {
     return (
         <Box className="App">
             <Header />
             <SearchArea />
+            <TakeNoteArea />
         </Box>
     );
 }
