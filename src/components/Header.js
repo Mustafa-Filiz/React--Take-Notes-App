b8402466690ca087df9f3d2ddc76d9ec6537238b
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-20 23:40:08 +0300

    header created

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+function Header() {
+	return (
+		<Typography variant="h2">
+			Take Notes
+		</Typography>
+	)
+}
+
+export default Header
