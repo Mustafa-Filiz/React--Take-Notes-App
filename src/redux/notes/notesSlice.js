d5d3e305fceb7b687821d8d3dee0d943ddcfb40c
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-20 23:40:00 +0300

    slice created

---
+++ src/redux/notes/notesSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const notesSlice = createSlice({
+    name: 'notes',
+    initialState: {},
+    reducers: {},
+});
+
+export default notesSlice.reducer;
