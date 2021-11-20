b08a3002253ddb13df7a00651e410fd6d22b1648
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-20 23:48:05 +0300

    search area imported

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Box } from '@mui/system';
 import './App.css';
 import Header from './components/Header';
+import SearchArea from './components/SearchArea';
 
 function App() {
     return (
         <Box className="App">
             <Header />
+            <SearchArea />
         </Box>
     );
 }
