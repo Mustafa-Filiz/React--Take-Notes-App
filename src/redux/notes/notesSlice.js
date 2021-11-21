1b8bb7c95eeb3cf2545ee60f0e225692f8d8304e
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-21 19:45:56 +0300

    local storage saved

--- src/redux/notes/notesSlice.js
+++ src/redux/notes/notesSlice.js
@@ -3,15 +3,19 @@ import { createSlice } from '@reduxjs/toolkit';
 export const notesSlice = createSlice({
     name: 'notes',
     initialState: {
-        items: [],
+        items: JSON.parse(localStorage.getItem("react-notes-app")),
+        searchText: '',
     },
     reducers: {
         addNote: (state, action) => {
             state.items.push(action.payload);
         },
+        searchNote: (state, action) => {
+            state.searchText = action.payload
+        },
     },
 });
 
-export const { addNote } = notesSlice.actions;
+export const { addNote, searchNote } = notesSlice.actions;
 
 export default notesSlice.reducer;
