84eeb7edfed6528d3d9e9b1534b7826505331390
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-21 19:45:37 +0300

    search comp updated

--- src/components/SearchArea.js
+++ src/components/SearchArea.js
@@ -1,12 +1,27 @@
-import { FormControl, TextField } from '@mui/material'
-import React from 'react'
+import { TextField } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { searchNote } from '../redux/notes/notesSlice';
 
 function SearchArea() {
-	return (
-		<FormControl>
-			<TextField variant="outlined" label="Search..." />
-		</FormControl>
-	)
+    const dispatch = useDispatch();
+    const [searchText, setSearchText] = useState('');
+
+    useEffect(() => {
+        dispatch(searchNote(searchText));
+    }, [dispatch, searchText]);
+
+    return (
+        <div style={{ margin: 'auto', width: '77%' }}>
+            <TextField
+                variant="outlined"
+                label="Search..."
+                fullWidth
+                value={searchText}
+                onChange={(e) => setSearchText(e.target.value)}
+            />
+        </div>
+    );
 }
 
-export default SearchArea
+export default SearchArea;
