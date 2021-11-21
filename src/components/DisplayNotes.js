130fd261bede6661c781c888c9964e07f6480a4f
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-21 19:45:21 +0300

    filter added

--- src/components/DisplayNotes.js
+++ src/components/DisplayNotes.js
@@ -1,6 +1,6 @@
 import { Box } from '@mui/system';
 import { makeStyles } from '@mui/styles';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import NoteCard from './NoteCard';
 
@@ -17,10 +17,17 @@ const useStyles = makeStyles({
 function DisplayNotes() {
     const classes = useStyles();
     const notes = useSelector((state) => state.notes.items);
+    const searchText = useSelector(state => state.notes.searchText)
+
+    const filteredNotes = notes.filter((note) => note.text.toLowerCase().includes(searchText.toLowerCase()))
+
+    useEffect(()=> {
+        localStorage.setItem("react-notes-app", JSON.stringify(notes))
+    }, [notes])
 
     return (
         <Box className={classes.container}>
-            {notes.map((note) => (
+            {filteredNotes.map((note) => (
                 <NoteCard key={note.id} note={note} />
             ))}
         </Box>
