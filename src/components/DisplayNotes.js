69fa26de7df13d503c00989daa05d68cbc14a5f4
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-21 15:48:48 +0300

    updates

--- src/components/DisplayNotes.js
+++ src/components/DisplayNotes.js
@@ -1,12 +1,30 @@
-import { Box } from '@mui/system'
-import React from 'react'
+import { Box } from '@mui/system';
+import { makeStyles } from '@mui/styles';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import NoteCard from './NoteCard';
+
+const useStyles = makeStyles({
+    container: {
+        width: '80%',
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-evenly',
+        margin: 'auto',
+    },
+});
 
 function DisplayNotes() {
-	return (
-		<Box>
-			Notes will be shown here.
-		</Box>
-	)
+    const classes = useStyles();
+    const notes = useSelector((state) => state.notes.items);
+
+    return (
+        <Box className={classes.container}>
+            {notes.map((note) => (
+                <NoteCard key={note.id} note={note} />
+            ))}
+        </Box>
+    );
 }
 
-export default DisplayNotes
+export default DisplayNotes;
